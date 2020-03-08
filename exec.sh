62c695fdfcea952b96485a62e09d149b69097df7
MIJ <mijinco1024@gmail.com>
2020-03-08 15:40:59 +0900

    update image 0.9.2.1 => 0.9.3.1

--- exec.sh
+++ exec.sh
@@ -11,5 +11,5 @@ docker run \
   --rm \
   -t \
   -v $(pwd)/userconfig:/userconfig/ \
-  techbureau/catapult-tools:gcc-keccak-0.9.2.1 \
+  techbureau/catapult-tools:gcc-0.9.3.1 \
   /usr/catapult/bin/catapult.tools.health --resources /userconfig/
